4d6a36339be95ea0324ad3708285523d8f3896d3
AndyAWD <anandydy529@gmail.com>
2020-09-01 18:16:58 +0800

    feat: 關閉程式功能完成

--- app/src/main/java/tw/com/andyawd/piratehegemonyonline/MainActivity.kt
+++ app/src/main/java/tw/com/andyawd/piratehegemonyonline/MainActivity.kt
@@ -2,10 +2,22 @@ package tw.com.andyawd.piratehegemonyonline
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initClickListener()
+    }
+
+    private fun initClickListener() {
+
+        mbAmExit.setOnClickListener {
+            android.os.Process.killProcess(android.os.Process.myPid())
+        }
     }
 }
