a53b56cd690fc74ac740ee1900101fe163db4826
AndyAWD <anandydy529@gmail.com>
2020-09-01 18:20:41 +0800

    feat: 反正我很閒水管首頁功能完成

--- app/src/main/java/tw/com/andyawd/piratehegemonyonline/MainActivity.kt
+++ app/src/main/java/tw/com/andyawd/piratehegemonyonline/MainActivity.kt
@@ -1,10 +1,16 @@
 package tw.com.andyawd.piratehegemonyonline
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val IGOTALLDAY_YOUTUBE = "https://www.youtube.com/channel/UCzjNxGvrqfxL9KGkObbzrmg"
+    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +21,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initClickListener() {
+
+        mbAmIgotalldayYoutube.setOnClickListener {
+            val intent = Intent()
+            intent.action = Intent.ACTION_VIEW
+            intent.data = Uri.parse(IGOTALLDAY_YOUTUBE)
+            startActivity(intent)
+        }
 
         mbAmExit.setOnClickListener {
             android.os.Process.killProcess(android.os.Process.myPid())
